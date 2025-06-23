bc102c91c348c85ceed50cbe434a4afd607bfa9f
FabianHenrick <fabianhernick@gmail.com>
2025-06-23 15:10:04 -0300

    my first steps in C# & .NET

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula01;
+// o  C# funciona atravez de classes, e possui uma sintaxe basica, que segue o seguinte padrão:
+
+//  class quer dizer "classe", que é como uma caixa onde colocamos códigos que funcionam juntos.
+
+// program é o nome dessa classe (poderia ser qualquer outro nome).
+
+// public significa que essa classe pode ser acessada por outras partes do programa.
+public class program
+
+
+
+// Essa linha define um método, ou seja, uma ação que o programa pode executar.
+
+// Main é o nome especial do método que sempre é o primeiro a ser executado quando você roda um programa em C#.
+
+// Vamos explicar cada palavra:
+
+// public: pode ser acessado de fora.
+
+// static: pertence à classe inteira, não a uma parte específica dela.
+
+// void: não retorna nenhum resultado.
+
+// Main(): o nome do método que roda primeiro.
+{
+  public static void Main()
+  { 
+    Console.WriteLine("Iniciando no .NET"); 
+  }
+}
