d19b004702ab3d49163fa2671e4f78068ec71042
FabianHenrick <fabianhernick@gmail.com>
2025-06-25 09:22:47 -0300

    aula-03: Input and Output

---
+++ aula-03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace aula03;
+
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("---------------CADASTRO DE USUÁRIOS---------------");
+    //Solicitar o nome do Usuario.
+    Console.WriteLine("Digite seu nome:");
+    string name = Console.ReadLine();
+    Console.WriteLine("Digite sua idade:");
+    int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite seu e-mail:");
+    string email = Console.ReadLine();
+
+    Console.WriteLine($"Seja bem vindo {name}, você tem {age} anos, seu email é : {email}");
+    
+  }
+}
