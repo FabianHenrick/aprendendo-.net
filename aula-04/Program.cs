020601701f8faf4d9acd901d79095972c77a9e81
FabianHenrick <fabianhernick@gmail.com>
2025-06-26 12:23:52 -0300

    aula-03: Arithmetic Operators

---
+++ aula-04/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace aula04;
+
+public class Program {
+
+public static void Main()
+{
+  Console.WriteLine("Seja bem vindo a caculadora mal programada");
+
+
+  Console.WriteLine("Digite o primeiro número da operação:");
+  int number1= Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Agora digite qual operação deseja fazer?");
+  Console.WriteLine("Para Soma: + | para Subtração: - | para Multiplicação: * | para Divisão: / ");
+  string sinOperator = Console.ReadLine();
+  Console.WriteLine("Digite o segundo número da operação:");
+  int number2= Convert.ToInt32(Console.ReadLine());
+
+  int result = 0;
+switch(sinOperator)
+{
+  case "+":
+    result = number1+number2;
+    break;
+  case "-":
+    result = number1 - number2 ;
+    break;
+  case "*":
+    result = number1 * number2;
+    break;
+  case "/" :
+    result = number1 / number2;
+    break;
+}
+ 
+
+  Console.WriteLine($"O resultado da operação {number1} {sinOperator} {number2} é: {result}");
+}
+
+}
