53ff24eda5cf27b18f8c23d6bd4b3d9f80da48ff
FabianHenrick <fabianhernick@gmail.com>
2025-06-28 17:40:09 -0300

    aula-05:conditional structures (if and else)

---
+++ aula-05/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace aula05;
+
+   public class Program
+   {
+
+      public static void Main()
+      {
+        Console.WriteLine("######### Verificador de Numeros Pares e Impares #########");
+        Console.WriteLine("Digite um número:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        if(number1 % 2 == 0 )
+        {
+          Console.WriteLine($"o número {number1} é par");
+        }else
+        {
+          Console.WriteLine($"o número {number1} é impar");
+        }
+      }
+
+    }
