1f614219679d79605dde2991839f1810e108bbea
FabianHenrick <fabianhernick@gmail.com>
2025-08-08 08:15:53 -0300

    aula-06: while

---
+++ aula-06/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula06;
+public class Program
+{
+  public static void Main()
+  {
+    int i = 0;
+
+    while (i <= 5)
+    {
+      Console.WriteLine($"O valor de I é: {i} " );
+      i = i + 1;
+    }
+  }
+}
