9232195f5199e84b6e628def5bbbba2423198a4a
FabianHenrick <fabianhernick@gmail.com>
2025-08-11 07:30:36 -0300

    aula-07: For Loop

---
+++ aula-07/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula07;
+
+public class Program
+{
+  public static void Main()
+  {
+    /*
+    for (condição 1; condição 2; condição 3;)
+
+    condição 1 é executada (uma única vez) antes da execução do bloco de código.
+
+    condição 2 define a condição para executar o bloco de código.
+
+    condição 3 é executado (todas as vezes) após o bloco de código ter sido executado.
+    */
+    //Ex1
+    for (int i = 1; i <= 10; i++)
+    {
+      Console.WriteLine(i);
+    }
+    //Ex2:
+    Console.WriteLine("######Tabela de Mutiplicação######");
+    for (int i = 1; i <= 10; i++)
+    {
+      for (int j = 1; j <= 10; j++)
+      {
+        Console.WriteLine($"{i} - {j} = {i * j},");
+      }
+    }
+  }
+}
